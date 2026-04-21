f72b21bb75c11170ba80555bb7539dc8f31e0096
javierah1987-byte <javierah1987@gmail.com>
2026-04-21 14:24:51 +0100

    Create layout.tsx

---
+++ app/admin/layout.tsx
@@ -0,0 +1,8 @@
+// @ts-nocheck
+export default function AdminLayout({ children }) {
+  return (
+    <div style={{ minHeight: '100vh', background: '#111', color: 'white' }}>
+      {children}
+    </div>
+  )
+}
